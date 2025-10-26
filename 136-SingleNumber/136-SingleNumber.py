db7033f6b0fca162543149d5f58d0b7c504e687b
Luce Tuuu <50800020+LuceTu@users.noreply.github.com>
2025-10-26 16:57:13 +0000

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.py
@@ -0,0 +1,11 @@
+# Last updated: 10/26/2025, 4:57:13 PM
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
