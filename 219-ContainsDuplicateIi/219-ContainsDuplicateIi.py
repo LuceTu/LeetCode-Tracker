b6c1404d3523dc80e9ca876445e7fe6d6f57c7f3
Luce Tuuu <50800020+LuceTu@users.noreply.github.com>
2025-10-26 16:57:11 +0000

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.py
@@ -0,0 +1,18 @@
+# Last updated: 10/26/2025, 4:57:11 PM
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        seen = set()
+        for i, num in enumerate(nums):
+            if num in seen:
+                return True
+            seen.add(num)
+            if len(seen)>k:
+                seen.remove(nums[i - k])
+        return False
+
+            
